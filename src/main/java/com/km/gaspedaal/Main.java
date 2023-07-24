1eb17551a5184ba2c23eabee5f1e9ab96de736c1
Kacper Marczewski <kacper.marczewski@gmail.com>
2023-07-24 22:54:59 +0200

    waiting time

--- src/main/java/com/km/gaspedaal/Main.java
+++ src/main/java/com/km/gaspedaal/Main.java
@@ -1,8 +1,5 @@
 package com.km.gaspedaal;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,17 +10,28 @@ public class Main {
     public static void main(String args[]) {
         String chromeDriverPath = "/Users/kacper/dev/chromedriver-mac-arm64/chromedriver";
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		WebDriver driver = new ChromeDriver(getOptions());
+		driver.get("https://www.autowereld.nl/goedkope-occasions");
+		filterAndPrint(driver);
+		driver.quit();
+    }
 
+	private static ChromeOptions getOptions() {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless", "--window-size=1920,1200","--ignore-certificate-errors", "--silent");
         options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
         options.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://www.autowereld.nl/goedkope-occasions");
-		
+		return options;
+	}
+
+	private static void filterAndPrint(WebDriver driver) {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		WebElement e = driver.findElement(By.id("search-results"));
 		System.out.println(e.getText());
-		
-		driver.quit();
-    }
+	}
 }
